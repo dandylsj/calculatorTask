949f95a4da8c5f122936ff31db150494712bc111
dandylsj <lsjyahoo@gmail.com>
2025-10-16 14:51:51 +0900

    Lv1 계산기 : switch 문 case 에서 println 하지 않고 결과값을 받은 후 switch 문 밖에서 결과 값 출력으로 변경

--- src/Lv1_calculator/Main.java
+++ src/Lv1_calculator/Main.java
@@ -18,19 +18,20 @@ public class Main {
             System.out.println("계산할 사칙연산 기호를 입력하세요(ex:+, -, *, /): ");
             char calculation = scanner.next().charAt(0); // char calculation = scanner.nextchar() 은 안되는것인가??
             // if (calculation == '+' || calculation == '-' || calculation == '*' || calculation == '/') {
+            double result = 0;//((double) num1 - num2); //초기화를 시켜준다.
                 switch (calculation) {
                     case ('+'):
-                        System.out.println("계산 결과는: "+ (num1 + num2));
+                        result = (num1 + num2);
                         break;
                     case ('-'):
-                        System.out.println("계산 결과는: "+ ((double) num1 - num2));
+                        result = ((double) num1 - num2);
                         break;
                     case ('*'):
-                        System.out.println("계산 결과는: "+ (num1 * num2));
+                        result = (num1 * num2);
                         break;
                     case ('/'):
                         if(num2 != 0) {
-                            System.out.println("계산 결과는: "+ ((double) num1 / num2));
+                        result = ((double) num1 / num2);
                         break;
                         }
                         else {
@@ -38,10 +39,14 @@ public class Main {
                         }
                     default: System.out.println("잘못된 입력값 입니다.");
                 }
-           // 계속해서 계산을 진행 할것인지에 대한 스캐너 입력
+                // 결과값을 출력해주기
+            System.out.println("결과: " + result);
+
+            // 계속해서 계산을 진행 할것인지에 대한 스캐너 입력
             scanner.nextLine();
             System.out.println("더 계산하시려면 1 ,종료하려면 exit 를 입력하세요");
             String ran = scanner.nextLine();
+
             // 계속해서 계산을 진행 할것인지에 대한 if 문 사용
             if (ran.equals("1")) {
                 continue;
