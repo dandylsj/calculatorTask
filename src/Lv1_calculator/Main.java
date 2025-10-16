0514175990a24381bdcbbe315577c5bb8274b228
dandylsj <lsjyahoo@gmail.com>
2025-10-16 14:46:00 +0900

    master 브랜치 > main 브렌치 변경 Lv1 계산기 : while 반복문 추가 하여 계속 계산 하기 기능, 불필요한 if 문 삭제,

--- src/Lv1_calculator/Main.java
+++ src/Lv1_calculator/Main.java
@@ -4,43 +4,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //스캐너 객체를 불러온다.
+        //스캐너 기능을 추가
         Scanner scanner = new Scanner(System.in);
 
-           // 첫번째 문자 입력 값 저장
+        while(true) {  //반복문을 사용하여 계속 계산
+            // 첫번째 계산할 숫자 저장하기
             System.out.println("첫번째로 입력할 숫자를 넣으세요: ");
             int num1 = scanner.nextInt();
-        // 두번째 숫자 입력 값 저장
+            // 두번째 계산할 숫자 저장하기
             System.out.println("두번째로 입력할 숫자를 넣으세요: ");
             int num2 = scanner.nextInt();
-            //사칙연산 기호를 저장
+            //사칙연산 기호 입력하고 저장하기
             System.out.println("계산할 사칙연산 기호를 입력하세요(ex:+, -, *, /): ");
             char calculation = scanner.next().charAt(0); // char calculation = scanner.nextchar() 은 안되는것인가??
-        //if 문을 사용하여 사칙연산 입력값을 대조
-            if (calculation == '+' || calculation == '-' || calculation == '*' || calculation == '/') {
-                //switch 문을 사용하여 기호에 맞는 결과를 실행
+            // if (calculation == '+' || calculation == '-' || calculation == '*' || calculation == '/') {
                 switch (calculation) {
                     case ('+'):
-                        System.out.println("계산 결과는: " + (num1 + num2));
+                        System.out.println("계산 결과는: "+ (num1 + num2));
                         break;
                     case ('-'):
-                        System.out.println("계산 결과는: " + ((double) num1 - num2));
+                        System.out.println("계산 결과는: "+ ((double) num1 - num2));
                         break;
                     case ('*'):
-                        System.out.println("계산 결과는: " + (num1 * num2));
+                        System.out.println("계산 결과는: "+ (num1 * num2));
                         break;
                     case ('/'):
-                        if (num2 != 0) {
-                            System.out.println("계산 결과는: " + ((double) num1 / num2));
-                            break;
-                        } else {
+                        if(num2 != 0) {
+                            System.out.println("계산 결과는: "+ ((double) num1 / num2));
+                        break;
+                        }
+                        else {
                             System.out.println("0은 입력할 수 없습니다.");
                         }
-                    default:
-                        System.out.println("잘못된 입력값 입니다.");
+                    default: System.out.println("잘못된 입력값 입니다.");
                 }
+           // 계속해서 계산을 진행 할것인지에 대한 스캐너 입력
+            scanner.nextLine();
+            System.out.println("더 계산하시려면 1 ,종료하려면 exit 를 입력하세요");
+            String ran = scanner.nextLine();
+            // 계속해서 계산을 진행 할것인지에 대한 if 문 사용
+            if (ran.equals("1")) {
+                continue;
+            } else if (ran.equals("exit")) {
+                System.out.println("종료합니다.");
+                System.exit(0);
             }
+
+
         }
     }
-
+}
 
